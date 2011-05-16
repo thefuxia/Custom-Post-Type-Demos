de83db70487cdc1de37eed719627630507865148
Thomas Scholz <info@toscho.de>
2011-05-16 15:58:45 +0200

    Move class Toscho_CPT_Base into a separate file.

---
+++ class.Toscho_CPT_Base.php
@@ -0,0 +1,14 @@
+<?php # -*- coding: utf-8 -*-
+/**
+ * Base class for a Custom Post Types
+ *
+ * @author Thomas Scholz <info@toscho.de>
+ * @version 1.1
+ */
+class Toscho_CPT_Base extends Toscho_CPT_And_Tax_Base
+{
+	public function extend_defaults()
+	{
+
+	}
+}
--- cpt-demo-5.php
+++ cpt-demo-5.php
@@ -59,19 +59,7 @@ class CPT5_Controller
 
 
 
-/**
- * Base class for a Custom Post Types
- *
- * @author Thomas Scholz <info@toscho.de>
- * @version 1.1
- */
-class Toscho_CPT_Base extends Toscho_CPT_And_Tax_Base
-{
-	public function extend_defaults()
-	{
 
-	}
-}
 
 
 /**
