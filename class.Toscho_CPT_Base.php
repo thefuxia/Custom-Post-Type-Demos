d04a4e497679f6b0dad117718bf2906014b3d6a9
Thomas Scholz <info@toscho.de>
2011-05-20 03:30:17 +0200

    Implement add_to_dashboard()

--- class.Toscho_CPT_Base.php
+++ class.Toscho_CPT_Base.php
@@ -11,4 +11,25 @@ class Toscho_CPT_Base extends Toscho_CPT_And_Tax_Base
 	{
 
 	}
+
+	/**
+	 * Show name and number in the Right Now dashboard widget.
+	 *
+	 * @return void
+	 */
+	public function add_to_dashboard()
+	{
+		$posts = wp_count_posts( $this->name );
+		$num   = number_format_i18n( $posts->publish );
+		$text  = _n( $this->labels['singular_name'], $this->labels['name'], $posts->publish );
+
+		// @todo map cap
+        if ( current_user_can( 'edit_pages' ) )
+        {
+            $num  = "<a href='edit.php?post_type=$this->name'>$num</a>";
+            $text = "<a href='edit.php?post_type=$this->name'>$text</a>";
+        }
+
+        $this->print_dashboard_row( $this->name, $num, $text );
+	}
 }
